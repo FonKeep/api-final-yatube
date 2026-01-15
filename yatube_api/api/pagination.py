f18f0bb81002f7b731daf78aa6c56f9b273144ce
Tikhon Karpukhin <fonkeep@mail.ru>
2026-01-15 21:23:29 +0300

    0.0.2

--- yatube_api/api/pagination.py
+++ yatube_api/api/pagination.py
@@ -1,12 +1,14 @@
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 
+
 class StandardResultsSetPagination(LimitOffsetPagination):
     default_limit = 5
 
     def get_paginated_response(self, data):
 
-        if self.request.query_params.get('limit') is None and self.request.query_params.get('offset') is None:
+        if (self.request.query_params.get('limit') is None
+                and self.request.query_params.get('offset') is None):
             return Response(data)
         else:
             return Response({
